cc5d437366c0d31414bb66e81514aef63268423d
indrajuni96 <indrajuniyanto96@gmail.com>
2019-09-28 11:47:51 +0700

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -8,11 +8,11 @@ const usersController = require('../controller/users')
 
 Route
     .get('/products', cors(), productsController.getProducts)
-    .get('/products/:idProduct', usersController.validateUser, productsController.getProductId)
-    .post('/products', usersController.validateUser, productsController.addProducts)
-    .put('/products/:idProduct', usersController.validateUser, productsController.editProduct)
-    .patch('/products/quantity/add/:idProduct', usersController.validateUser, productsController.addProductQuantity)
-    .patch('/products/quantity/reduce/:idProduct', usersController.validateUser, productsController.reduceProductQuantity)
-    .delete('/products/:idProduct', usersController.validateUser, productsController.deleteProduct)
+    .get('/products/:idProduct', productsController.getProductId)
+    .post('/products', productsController.addProducts)
+    .put('/products/:idProduct', productsController.editProduct)
+    .patch('/products/quantity/add/:idProduct', productsController.addProductQuantity)
+    .patch('/products/quantity/reduce/:idProduct', productsController.reduceProductQuantity)
+    .delete('/products/:idProduct', productsController.deleteProduct)
 
 module.exports = Route
