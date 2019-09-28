ba79fdda461366446b6642fe1c96749ec234210e
indrajuni96 <indrajuniyanto96@gmail.com>
2019-09-28 11:47:48 +0700

    Update categories.js

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -8,8 +8,8 @@ const usersController = require('../controller/users')
 Route
     .get('/categories', categoriesController.getCategories)
     .get('/categories/:idCategori', categoriesController.getCategori)
-    .post('/categories/', usersController.validateUser, categoriesController.addCategori)
-    .put('/categories/:idCategori', usersController.validateUser, categoriesController.editCategori)
-    .delete('/categories/:idCategori', usersController.validateUser, categoriesController.deleteCategori)
+    .post('/categories/', categoriesController.addCategori)
+    .put('/categories/:idCategori', categoriesController.editCategori)
+    .delete('/categories/:idCategori', categoriesController.deleteCategori)
 
 module.exports = Route
